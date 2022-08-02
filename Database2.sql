aa2cb0ba0c5032369466556c6308e7d18647473a
ManarAli1 <108229843+ManarAli1@users.noreply.github.com>
2022-08-02 22:45:27 +0300

    Create Database2.sql

---
+++ Database2.sql
@@ -0,0 +1,9 @@
+create table RobotMovement(
+        ID_R char (10) not null,
+        Up char (10) not null,
+        Down char (10) not null,
+        Left1 char (10) not null,
+        Right1 char (10) not null,
+        Center char (10) not null,
+         primary key (ID_M)
+	);
