41b55e25ee6dbd34bc37d3a819d3f6ab7c2bfcc0
ManarAli1 <108229843+ManarAli1@users.noreply.github.com>
2022-08-08 02:28:18 +0300

    Update Database2.sql

--- Database2.sql
+++ Database2.sql
@@ -1,9 +1,12 @@
-create table RobotMovement(
-        ID_R char (10) not null,
-        Up char (10) not null,
-        Down char (10) not null,
-        Left1 char (10) not null,
-        Right1 char (10) not null,
-        Center char (10) not null,
-         primary key (ID_M)
-	);
+CREATE TABLE `robotmotion` (
+  `Id` int(11) NOT NULL,
+  `motion` varchar(1) NOT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `robotmotion`
+  ADD PRIMARY KEY (`Id`);
+  
+  ALTER TABLE `robotmotion`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
